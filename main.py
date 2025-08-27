3d4769908815f5661be1599d15a158e83d79e67d
Aitaneuh <ethanb9407@gmail.com>
2025-08-27 08:23:08 +0200

    can load data

---
+++ main.py
@@ -0,0 +1,14 @@
+﻿import pandas as pd
+import json
+
+with open('data/data.json', 'r') as f:
+    data = json.load(f)
+
+columns = [
+    "extra", "civility", "lastname", "firstname", "middle", 
+    "address", "zip", "city", "canton", "email"
+]
+
+df = pd.DataFrame(data["data"], columns=columns)
+
+print(df)
